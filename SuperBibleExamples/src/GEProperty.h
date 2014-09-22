d45bf411e7794db7a21f750038f55ad81cf0e7b0
Lehm2000 <jeff@jamodeler.com>
2014-09-22 14:01:31 +1000

    Initial branch commit.  Started to fill in GEProperty

--- SuperBibleExamples/src/GEProperty.h
+++ SuperBibleExamples/src/GEProperty.h
@@ -20,9 +20,9 @@ private:
 public:
 
 	// Structors
-	GEProperty()
-	GEProperty( T value )
-	GEProperty( GEProperty& source )
+	GEProperty();
+	GEProperty( T value );
+	GEProperty( GEProperty& source );
 	~GEProperty();
 
 	// Setters
@@ -41,5 +41,73 @@ public:
 	void setControllerGameEntitiesPointer( const std::map< std::string, GEObject* >* gameEntities);
 	
 };
+
+// Structors
+
+template <class T>
+GEProperty<T>::GEProperty()
+{
+}
+
+template <class T>
+GEProperty<T>::GEProperty( T value )
+{
+}
+
+template <class T>
+GEProperty<T>::GEProperty( GEProperty& source )
+{
+}
+
+template <class T>
+GEProperty<T>::~GEProperty();
+
+// Setters
+
+template <class T>
+void GEProperty<T>::setValue( const T value )
+{
+}
+
+
+// Getters
+
+
+template <class T>
+T GEProperty<T>::getBaseValue() const
+{
+}
+
+template <class T>
+T GEProperty<T>::getControlledValue() const
+{
+}
+
+
+// Operators
+
+template <class T>
+GEProperty& GEProperty<T>::operator=( const GEProperty& source )
+{
+}
+
+
+// Functions
+
+template <class T>
+void GEProperty<T>::addPositionController( GEController<T>* controller )
+{
+}
+
+template <class T>
+void GEProperty<T>::removePositionController( const unsigned int index )
+{
+}
+
+template <class T>
+void GEProperty<T>::setControllerGameEntitiesPointer( const std::map< std::string, GEObject* >* gameEntities )
+{
+}
+
 
 #endif /* GEPROPERTY_H */
